be3325b3ce143e9ae0d92110d242a7150befd952
PraiseTheGoldenHoodie <34354911+PraiseTheGoldenHoodie@users.noreply.github.com>
2018-08-26 09:25:25 -0500

    Add project files.

---
+++ String Inserter Y/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace String_Inserter_Y
+{
+    partial class StringInserterY
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxInsert = new System.Windows.Forms.TextBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chooseFile = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.checkBoxOverride = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxStart
+            // 
+            this.textBoxStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxStart.Location = new System.Drawing.Point(70, 83);
+            this.textBoxStart.Name = "textBoxStart";
+            this.textBoxStart.Size = new System.Drawing.Size(375, 22);
+            this.textBoxStart.TabIndex = 0;
+            this.textBoxStart.Text = "C06 R";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "End:";
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEnd.Location = new System.Drawing.Point(70, 111);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(375, 22);
+            this.textBoxEnd.TabIndex = 2;
+            this.textBoxEnd.Text = "C0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Insert*:";
+            // 
+            // textBoxInsert
+            // 
+            this.textBoxInsert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInsert.Location = new System.Drawing.Point(70, 139);
+            this.textBoxInsert.Name = "textBoxInsert";
+            this.textBoxInsert.Size = new System.Drawing.Size(375, 22);
+            this.textBoxInsert.TabIndex = 4;
+            this.textBoxInsert.Text = "C061 R_C_D 0 0\\n";
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(15, 167);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(90, 42);
+            this.buttonExecute.TabIndex = 6;
+            this.buttonExecute.Text = "Execute";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chooseFile
+            // 
+            this.chooseFile.Location = new System.Drawing.Point(12, 12);
+            this.chooseFile.Name = "chooseFile";
+            this.chooseFile.Size = new System.Drawing.Size(107, 32);
+            this.chooseFile.TabIndex = 7;
+            this.chooseFile.Text = "Choose File...";
+            this.chooseFile.UseVisualStyleBackColor = true;
+            this.chooseFile.Click += new System.EventHandler(this.chooseFile_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFile.Enabled = false;
+            this.textBoxFile.Location = new System.Drawing.Point(125, 17);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.ReadOnly = true;
+            this.textBoxFile.Size = new System.Drawing.Size(320, 22);
+            this.textBoxFile.TabIndex = 8;
+            this.textBoxFile.TabStop = false;
+            // 
+            // checkBoxOverride
+            // 
+            this.checkBoxOverride.AutoSize = true;
+            this.checkBoxOverride.Location = new System.Drawing.Point(12, 50);
+            this.checkBoxOverride.Name = "checkBoxOverride";
+            this.checkBoxOverride.Size = new System.Drawing.Size(154, 21);
+            this.checkBoxOverride.TabIndex = 9;
+            this.checkBoxOverride.Text = "override original file";
+            this.checkBoxOverride.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "*use \\n to represent newline";
+            // 
+            // StringInserterY
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 276);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBoxOverride);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.chooseFile);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxInsert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStart);
+            this.Name = "StringInserterY";
+            this.Text = "String Inserter Y";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxInsert;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button chooseFile;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.CheckBox checkBoxOverride;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ String Inserter Y/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace String_Inserter_Y
+{
+    public partial class StringInserterY : Form
+    {
+        string dir;
+        string input;
+        string textInsert;
+        public StringInserterY()
+        {
+            InitializeComponent();
+            dir = "";
+            input = "";
+        }
+
+        private void chooseFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text file | *.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dir = openFileDialog1.FileName;
+                textBoxFile.Text = dir;
+                input = File.ReadAllText(dir);
+            }
+        }
+
+        private void buttonExecute_click(object sender, EventArgs e)
+        { //Execute AKA beginning of run
+            input = File.ReadAllText(dir);
+            textInsert = textBoxInsert.Text;
+            textInsert = textInsert.Replace(@"\n", "\n");
+            textInsert = textInsert.Replace(@"\r", "\r");
+            textInsert = textInsert.Replace(@"\t", "\t");
+            buttonExecute.Enabled = false;
+            int index = 0;
+            while(input.Substring(index).Contains(textBoxStart.Text))
+            {
+                index = input.Substring(index).IndexOf(textBoxStart.Text) + textBoxStart.Text.Length + index;
+                index += textBoxStart.Text.Length;
+                string inputPastStart = input.Substring(index);
+                int insertAt = inputPastStart.IndexOf(textBoxEnd.Text) + index;
+                input = input.Insert(insertAt, textInsert);
+                index += textInsert.Length;
+            }
+            Console.WriteLine("starting write to file...");
+            string writeToDir;
+            if (checkBoxOverride.Checked)
+            {
+                writeToDir = dir;
+            }
+            else
+                writeToDir = dir.Substring(0, dir.LastIndexOf("\\") + 1) + "output.txt";
+            File.Delete(writeToDir);
+            File.AppendAllText(writeToDir, input);
+            MessageBox.Show("Completed.");
+            buttonExecute.Enabled = true;
+        }
+
+    }
+}
